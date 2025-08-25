ffe87191e9b7308b2b1d60fec578bb28c2de58b6
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-08-25 15:13:55 +0000

    Better build script :sob:

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-mkdir -p out/
-fasm main.s
+#!/usr/bin/env bash
+
+
+if [ ! -d out ]; then
+  mkdir out
+  echo "Created output directory"
+fi
+fasm main.s >&/dev/null
+echo "Compiled assembly code"
 mv main out/
+echo "Copied executable to output directory"
