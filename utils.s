1d6f938e91cc68689536e8c49e3cd1a97598d344
Domenicopezzullo <pezzullodomenico712@gmail.com>
2025-08-25 14:53:54 +0000

    Outdated comments

--- utils.s
+++ utils.s
@@ -1,20 +1,17 @@
-; Input:
-;   rbx = pointer to string (file_name)
-;   rcx = max length (bytes read from input)
 remove_newline:
     push rdi
     push rcx
 
-    xor rdi, rdi  ; index = 0
+    xor rdi, rdi 
 
 .loop:
     cmp rdi, rcx
     jge .done
 
     mov al, byte [rbx + rdi]
-    cmp al, 0x0A       ; newline?
+    cmp al, 0x0A
     je .replace
-    cmp al, 0          ; null terminator?
+    cmp al, 0
     je .done
 
     inc rdi
